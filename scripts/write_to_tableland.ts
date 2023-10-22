367064862bbfeb411b76fb1c7b752ae7ea5a631c
Gabriel Fior <fior.gbm@gmail.com>
2023-10-22 01:29:49 -0400

    Added Infura as base provider

--- scripts/write_to_tableland.ts
+++ scripts/write_to_tableland.ts
@@ -1,3 +1,4 @@
+import {ethers} from 'ethers';
 import { Database } from "@tableland/sdk";
 import { Wallet, getDefaultProvider } from "ethers";
 import {config} from 'dotenv';
@@ -8,12 +9,14 @@ const randomInt = (min: number = 1, max: number = 100000000000) =>
 
 async function main() {
     console.log('start');
+    //const provider = new ethers.providers.InfuraProvider(process.env.SEPOLIA_RPC_URL, process.env.INFURA_API_KEY);
 
     const privateKey = process.env.PRIVATE_KEY_FOR_TABLELAND!;
     const wallet = new Wallet(privateKey);
     // To avoid connecting to the browser wallet (locally, port 8545).
     // For example: "https://polygon-mumbai.g.alchemy.com/v2/YOUR_ALCHEMY_KEY"
-    const provider = getDefaultProvider(process.env.SEPOLIA_RPC_URL);
+    const provider = getDefaultProvider(process.env.SEPOLIA_RPC_URL, {infura: process.env.INFURA_API_KEY});
+
     const signer = wallet.connect(provider);
     // Connect to the database
     const db = new Database({ signer });
@@ -26,7 +29,7 @@ async function main() {
     const { meta: insert } = await db
         .prepare(`INSERT INTO ${tableName} (id, campaign_name, amount_dai, start_date, end_date) 
         VALUES (?,?,?,?,? );`)
-        .bind(randomInt(), "test_campaign","10_000_000_000_000_000_000","2023-01-01 00:00:00","2023-01-02 00:00:00" )
+        .bind(randomInt(), "test_campaign","10_000_000_000_000_000_000",new Date().toISOString(),"2024-01-02 00:00:00" )
         .run();
 
     // Wait for transaction finality
